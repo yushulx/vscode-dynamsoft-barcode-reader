2c8ac1d601440d8ebc3f1eed819b78777d62bfd5
yushulx <lingxiao1002@gmail.com>
2022-05-17 17:29:58 +0800

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# vscode-dbr-code-generator
+# Code Generator for Dynamsoft Barcode SDK 
+
+The Code Generator helps developers to quickly get started with [Dynamsoft Barcode Reader SDK](https://www.dynamsoft.com/barcode-reader/overview/) using JavaScript, Python, C/C++, .NET, Android, and iOS in Visual Studio Code.
+
+## Dev Requirements for Windows, Linux and macOS
+- [Visual Studio](https://visualstudio.microsoft.com/downloads/)
+- [CMake](https://cmake.org/download/)
+- [.NET SDK](https://dotnet.microsoft.com/en-us/download/visual-studio-sdks)
+- [Python](https://www.python.org/downloads/)
+- [Android Studio](https://developer.android.com/studio)
+- [Xcode](https://developer.apple.com/xcode/)
+
+## Usage
+1. Press `F1` to show command palette and run **DBR** to list all supported projects.
+    
+    ![vscode-dbr-extension](https://www.dynamsoft.com/codepool/img/2022/05/vscode-dbr-extension.png)
+
+2. Select the project you want to generate code for.
+3. Build and run the project:
+    - **Web**
+        ```bash
+        python -m http.server
+        ```
+    - **Python** (**Windows**, **Linux**, and **macOS**)
+        ```bash
+        pip install dbr
+        python app.py
+        ```
+    - **.NET** (**Windows**, **Linux**, and **macOS**)
+        ```bash
+        dotnet restore
+        dotnet run
+        ```
+    - **C/C++** (**Windows**, **Linux**, and **macOS**)
+        ```bash
+        mkdir build
+        cd build
+
+        # if Windows
+        cmake -DCMAKE_GENERATOR_PLATFORM=x64 ..
+        # else
+        cmake ..
+
+        cmake --build .
+        ```
+    - **Android**
+        
+        Import the project to Android Studio.
+
+    - **iOS**
+        
+        Run `pod install` and import the project to Xcode.
+
+
---
+++ src/extension.ts
@@ -0,0 +1,109 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { Uri } from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const samples = {
+	"python": path.join(__dirname, '../res/python/'),
+	"dotnet": path.join(__dirname, '../res/dotnet/'),
+	"cpp": path.join(__dirname, '../res/cpp/'),
+	"web": path.join(__dirname, '../res/web/'),
+	"android": path.join(__dirname, '../res/android/'),
+	"ios": path.join(__dirname, '../res/ios/')
+};
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	context.subscriptions.push(...[
+		vscode.commands.registerCommand('dbr.dotnet', async () => {
+			createProject(samples['dotnet']);
+		}),
+		vscode.commands.registerCommand('dbr.cpp', async () => {
+			createProject(samples['cpp']);
+		}),
+		vscode.commands.registerCommand('dbr.web', async () => {
+			createProject(samples['web']);
+		}),
+		vscode.commands.registerCommand('dbr.python', async () => {
+			createProject(samples['python']);
+		}),
+		vscode.commands.registerCommand('dbr.android', async () => {
+			createProject(samples['android']);
+		}),
+		vscode.commands.registerCommand('dbr.ios', async () => {
+			createProject(samples['ios']);
+		}),
+		vscode.commands.registerCommand('dbr.doc', async () => {
+			vscode.env.openExternal(vscode.Uri.parse('https://www.dynamsoft.com/barcode-reader/introduction/?ver=latest'));
+		})
+	]);
+}
+
+function copyFolderSync(src: string, des: string) {
+	fs.readdir(src, (err, files) => {
+		if (err) {
+			return;
+		}
+
+		files.forEach(file => {
+			let srcFile = path.join(src, file);
+			let desFile = path.join(des, file);
+			if (fs.statSync(srcFile).isDirectory()) {
+				fs.mkdirSync(desFile);
+				copyFolderSync(srcFile, desFile);
+			} else {
+				fs.copyFileSync(srcFile, desFile);
+			}
+		});
+	});
+}
+
+async function openFolder() {
+	const projectName = await vscode.window.showInputBox({
+		prompt: 'Enter a name for the new project',
+		validateInput: (value: string): string => {
+			if (!value.length) {
+				return 'A project name is required';
+			}
+			return '';
+		}
+	});
+	if (!projectName) {
+		return '';
+	}
+
+	let workspace = '';
+	const folderUris = await vscode.window.showOpenDialog({ canSelectFolders: true, canSelectFiles: false, canSelectMany: false, openLabel: 'Select folder' });
+	if (!folderUris) {
+		return '';
+	}
+
+	let workspaceFolderUri = folderUris[0];
+	console.log(workspaceFolderUri);
+	vscode.commands.executeCommand("vscode.openFolder", workspaceFolderUri);
+	vscode.window.showInformationMessage(workspaceFolderUri.fsPath);
+	workspace = workspaceFolderUri.fsPath;
+	let projectFolder = path.join(workspace, projectName);
+	if (!fs.existsSync(projectFolder)) {
+		fs.mkdirSync(projectFolder);
+	}
+
+	vscode.commands.executeCommand("vscode.openFolder", Uri.file(projectFolder));
+
+	return projectFolder;
+}
+
+async function createProject(src: string) {
+	
+	const folderPath = await openFolder();
+	if (folderPath !== '') {
+		copyFolderSync(src, folderPath);
+	}
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
